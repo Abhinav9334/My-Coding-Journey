e300603736544e7b0a3ba73cac0cdb17ac8e9984
Abhinav9334 <abhinavkrjha9334@gmail.com>
2024-08-22 16:25:25 +0530

    Add files via upload

---
+++ program3.java
@@ -0,0 +1,10 @@
+package studyopedia;
+
+public class program3 {
+	public static void main(String args []) {
+		int human_age;
+		int dog_age = 2;
+		human_age = dog_age *7;
+		System.out.println("The human age is"+ human_age);
+	}
+}
